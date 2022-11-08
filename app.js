c5f70b7dbce35233240a8dbb9dd8deb9b015d90f
artinring <60808529+artinring@users.noreply.github.com>
2022-11-08 23:19:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
+if(!navigator.mediaDevices) {
+    document.querySelector('#js-unsupported').classList.add('is-show')
+}
+
+if(window.BarcodeDetector == undefined) {
+    console.log('Barcorde Detector is not supported by this browser.')
+    document.querySelector('#js-unsupported').classList.add('is-show')
+}
+
 const video = document.getElementById("js-video")
+
+const checkImage = () => {
+    const barcodeDetector = new BarcodeDetector()
+    barcodeDetector
+     .detect(video)
+     .then((barcodes) => {
+        if (barcodes.length > 0) {
+            for (let barcode of barcoodes) {
+                openModal(barcode.rawValue)
+            }
+        } else {
+            setTimeout(() => {
+                checkImage()
+            }, 200)
+        }
+     })
+     .catch((e) => {
+        console.error('Barcodde Detection failed.')
+     })
+}
 
 navigator.mediaDevices
     .getUserMedia({
@@ -9,6 +38,7 @@ navigator.mediaDevices
         video.srcObject = stream
         video.onloadedmetadata = function(e) {
             video.play()
+            checkImage()
         }
     })
     .catch(function(err) {
